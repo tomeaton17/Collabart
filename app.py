c55e29fc0ee7b7f1bbc307fe0a9e796f89b48094
Tom Eaton <thomfurpwns@gmail.com>
2017-10-26 16:12:58 +0100

    Added users table to db

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 @app.route('/')
 def index():
