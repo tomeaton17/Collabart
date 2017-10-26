ca20e89bb595724ecda523306a7591641f385a6a
Tom Eaton <thomfurpwns@gmail.com>
2017-10-26 15:52:45 +0100

    Added sqlite db connection

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, json
-import base64, sqlite3
+from flask_sqlalchemy import SQLAlchemy
+import base64, os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,
+                                                      'store/database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
 
 @app.route('/')
 def index():
