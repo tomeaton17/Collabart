4380a32fcb850a5a5b438b47745423994171677c
Tom Eaton <thomfurpwns@gmail.com>
2017-10-26 16:23:19 +0100

    Added Painting table to database

--- app.py
+++ app.py
@@ -20,6 +20,14 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % self.username
 
+class Painting(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return '<Painting %r>' % self.name
+
 @app.route('/')
 def index():
     return render_template('index.html', test='test')
