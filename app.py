cebf20dec2bb8baaf325ca4025996b01d803caba
Tom Eaton <thomfurpwns@gmail.com>
2017-10-26 13:00:52 +0100

    Added ajax test route

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html', test='test')
+
+@app.route('/ajax', methods=['POST'])
+def ajax():
+    data = request.args
+    print(data)
+    return data
 
 if __name__ == '__main__':
     app.run(debug='True')
